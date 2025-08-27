2118bf67ec8f2b93834ac7e64e544244a5c33178
Malin Sundberg <anomalins@gmail.com>
2025-08-27 14:46:31 +0200

    fixing some css

--- src/Card.tsx
+++ src/Card.tsx
@@ -36,11 +36,11 @@ export default function Card({ card, onDelete }: CardProps) {
             <button className="delete-button" onClick={handleDelete}><img src={close} alt="close" /></button>
             <p className="message">{card.message}</p>
             {card.link && (<p className="link"><a href={card.link} target="_blank" rel="noopener noreferrer">{card.link}</a></p>)}
-            <p className="name"><strong>{card.name}</strong></p>
-            <label className="check">
+            <div className="lastrow"> <label className="check">
                 <input type="checkbox" checked={isWatched} onChange={toggleWatched} />
                 <span><small>Vill inte se (igen)</small></span>
-            </label>
+            </label><p className="name"><strong>{card.name}</strong></p>
+           </div>
         </div>
     )
 }
